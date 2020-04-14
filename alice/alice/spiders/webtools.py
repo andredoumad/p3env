bbc524a906b7c8eef7c9e7614a4a0369b8100025
andre <andre@stringkeeper.com>
2020-04-13 17:35:10 -0700

    tr3b

--- alice/alice/spiders/webtools.py
+++ alice/alice/spiders/webtools.py
@@ -1398,6 +1398,39 @@ class WebTools:
                                         # process emails start 
                                         emails = []
                                         emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", str(item))
+                                        
+                                        
+                                        def append_language(noun_phrase='-', lemma='-', pos='-', entity_text='-', label='-'):
+                                            # eventlog('append_language: ' + str(url) + ',' + str(item) + ',' + str(noun_phrase) + ',' + str(lemma) + ',' + str(pos) + ',' + str(entity_text) + ',' + str(label))
+                                            # if str(socket.gethostname()) == "tr3b":
+                                            string_content = str(url).replace(",", "-") + ',' + str(item).replace(",", "-") + ',' + str(noun_phrase).replace(",", "-") + ',' + str(lemma).replace(",", "-") + ',' + str(pos).replace(",", "-") + ',' + str(entity_text).replace(",", "-") + ',' + str(label).replace(",", "-")
+                                            if label != '-':
+                                                record_message = {
+                                                    'csv_index': 'placeholder',
+                                                    'csv_url': str(url).replace(",", "-"),
+                                                    'csv_sentence': str(item).replace(",", "-"),
+                                                    'csv_noun_chunk': str(noun_phrase).replace(",", "-"),
+                                                    'csv_lemma': str(lemma).replace(",", "-"),
+                                                    'csv_pos': str(pos).replace(",", "-"),
+                                                    'csv_text': str(entity_text).replace(",", "-"),
+                                                    'csv_label': str(label).replace(",", "-")
+                                                }
+                                            # record_message['csv_index'] = 0
+                                            # record_message['csv_url'] = str(url)
+                                            # record_message['csv_sentence'] = str(item)
+                                            # record_message['csv_noun_chunk'] = noun_phrase
+                                            # record_message['csv_lemma'] = lemma
+                                            # record_message['csv_pos'] = pos
+                                            # record_message['csv_text'] = entity_text
+                                            # record_message['csv_label'] = label
+
+                                            with open(processed_language_document_filepath, 'a+') as iwrite:
+                                                if label != '-':
+                                                    self.charlotte.alice.send_message(record_message, 'a_csv')
+        
+                                                iwrite.write(string_content)
+                                                iwrite.write('\n')
+
                                         if emails:
                                             for email in emails:
                                                 valid_email = True
@@ -1426,6 +1459,7 @@ class WebTools:
                                                             iwrite.write(str(email))
                                                             iwrite.write('\n')
                                                             _HARVEST_COUNT += 1
+                                                        append_language(entity_text=str(email), label='EMAIL')
                                                         eventlog('FOUND EMAIL: ' + str(email) + ' Total: ' + str(emailcount))
                                                         # self.charlotte.alice.send_message(' | EMAIL | ' + str(email) + ' ', 'print')
                                                         
@@ -1469,6 +1503,8 @@ class WebTools:
                                                 searching = False                                            
                                             return found
 
+
+
                                         # Process whole documents
                                         if not os.path.exists(str(str(Path.home()) + '/p3env/alice/alice/spiders/DATABASE/JOBS/' + str(job_name) + '/harvest/spacy/' + str(directory_key) + '/')):
                                             os.makedirs( str(str(Path.home()) + '/p3env/alice/alice/spiders/DATABASE/JOBS/' + str(job_name) + '/harvest/spacy/' + str(directory_key) + '/'))
@@ -1479,36 +1515,7 @@ class WebTools:
                                                 w.write('URL,Sentence,Noun Chunk,Lemma,POS,Entity Text,Label')
                                                 w.write('\n')
 
-                                        def append_language(noun_phrase='-', lemma='-', pos='-', entity_text='-', label='-'):
-                                            # eventlog('append_language: ' + str(url) + ',' + str(item) + ',' + str(noun_phrase) + ',' + str(lemma) + ',' + str(pos) + ',' + str(entity_text) + ',' + str(label))
-                                            # if str(socket.gethostname()) == "tr3b":
-                                            string_content = str(url).replace(",", "-") + ',' + str(item).replace(",", "-") + ',' + str(noun_phrase).replace(",", "-") + ',' + str(lemma).replace(",", "-") + ',' + str(pos).replace(",", "-") + ',' + str(entity_text).replace(",", "-") + ',' + str(label).replace(",", "-")
-                                            if label != '-':
-                                                record_message = {
-                                                    'csv_index': 'placeholder',
-                                                    'csv_url': str(url).replace(",", "-"),
-                                                    'csv_sentence': str(item).replace(",", "-"),
-                                                    'csv_noun_chunk': str(noun_phrase).replace(",", "-"),
-                                                    'csv_lemma': str(lemma).replace(",", "-"),
-                                                    'csv_pos': str(pos).replace(",", "-"),
-                                                    'csv_text': str(entity_text).replace(",", "-"),
-                                                    'csv_label': str(label).replace(",", "-")
-                                                }
-                                            # record_message['csv_index'] = 0
-                                            # record_message['csv_url'] = str(url)
-                                            # record_message['csv_sentence'] = str(item)
-                                            # record_message['csv_noun_chunk'] = noun_phrase
-                                            # record_message['csv_lemma'] = lemma
-                                            # record_message['csv_pos'] = pos
-                                            # record_message['csv_text'] = entity_text
-                                            # record_message['csv_label'] = label
 
-                                            with open(processed_language_document_filepath, 'a+') as iwrite:
-                                                if label != '-':
-                                                    self.charlotte.alice.send_message(record_message, 'a_csv')
-        
-                                                iwrite.write(string_content)
-                                                iwrite.write('\n')
 
                                         doc = nlp(item)
 

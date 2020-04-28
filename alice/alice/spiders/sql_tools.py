72f796732efd1223d376fcc4cb7cceedb82feb0b
Andre Doumad <andredoumad@gmail.com>
2020-04-28 09:38:49 -0700

    gman

--- alice/alice/spiders/sql_tools.py
+++ alice/alice/spiders/sql_tools.py
@@ -46,8 +46,8 @@ if __name__ == "__main__":
 
     # sql = "SELECT m.dept_no, m.emp_no, d.dept_name FROM dept_manager_dup m INNER JOIN departments_dup d ON m.dept_no = d.dept_no ORDER BY m.dept_no;"
 
-    # Extract a list containing information about all managers’ employee number, first and last name, department number, and hire date. 
-    sql = "SELECT e.emp_no, e.first_name, e.last_name, dm.dept_no, e.hire_date FROM employees e JOIN dept_manager dm ON e.emp_no = dm.emp_no;"
+    # # Extract a list containing information about all managers’ employee number, first and last name, department number, and hire date.
+    # sql = "SELECT e.emp_no, e.first_name, e.last_name, dm.dept_no, e.hire_date FROM employees e JOIN dept_manager dm ON e.emp_no = dm.emp_no;"
 
     mycursor.execute(sql)
     for x in mycursor:
